342e5dfca0e81c5516979dc42eb297c730a6a136
vsaxena12 <vsaxena1@binghamton.edu>
2021-04-22 06:17:35 -0400

    2D Arrays

---
+++ src/TwoDimensionalArrays/TwoDD.java
@@ -0,0 +1,59 @@
+package TwoDimensionalArrays;
+import java.util.Scanner;
+
+public class TwoDD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Decide the Rows");
+		int rows = scan.nextInt();
+		
+		
+		System.out.println("Decide the Columns");
+		int columns = scan.nextInt();
+		
+		System.out.println("\nRows: "+rows);
+		System.out.println("\nColumns: "+columns);
+		
+		//Declare 2D Array
+		int[][] array = new int[rows][columns];
+		
+		
+		//Get the length of the 2-D array
+		int length = array.length;
+		System.out.println("Array Length: "+length); //It shows length of the String
+		
+		length = array[0].length;
+		System.out.println("Array Length: "+length); //It shows length of the String
+		
+		int count = 0;
+		
+		for(int i=0; i<rows; i++)
+		{
+			for(int j=0; j<columns; j++)
+			{
+				array[i][j] = count;
+				System.out.print(array[i][j]+" ");
+				count++;
+				
+			}
+			System.out.println();
+		}
+		
+		int [][] newArray = new int[2][];
+		newArray[0] = new int[5];
+		newArray[1] = new int[2];
+		
+		for(int i=0; i<2; i++)//find which rows are associated with which columns
+		{
+			System.out.print("Length of "+i+"th rows: "+newArray[i].length+" ");
+			//System.out.println();
+		}
+		
+		length = newArray[0].length;
+		System.out.println("Array Length: "+length);
+		
+	}
+
+}
